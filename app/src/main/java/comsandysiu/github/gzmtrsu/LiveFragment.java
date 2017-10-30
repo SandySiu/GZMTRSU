d460d2b7e25ad861f35135f7d20b26bc63f3cdb9
Sandy Siu <gzmtr_line6@126.com>
2017-10-30 23:51:07 +0800

    webview_live commit

--- app/src/main/java/comsandysiu/github/gzmtrsu/LiveFragment.java
+++ app/src/main/java/comsandysiu/github/gzmtrsu/LiveFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
 
 /**
  * Created by Sandy on 2017/10/28.
@@ -12,10 +13,16 @@ import android.view.ViewGroup;
 
 public class LiveFragment extends Fragment {
 
+    private WebView liveWeb;
+
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                               Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_live, container, false);
+        View view = inflater.inflate(R.layout.fragment_live, container, false);
+        liveWeb = (WebView)view.findViewById(R.id.webview_live);
+        liveWeb.loadUrl("http://ydyc.gzmtr.cn:13050/");
+
+        return view;
     }
 
 }
